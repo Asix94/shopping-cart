7c0d74c7d4633fe7e8157fb26b661a0fd72973c4
Carlos Ramos <carlosrt1994@gmail.com>
2024-05-12 10:18:12 +0200

    feat: create cart table migratin

---
+++ migrations/Version20240512081428.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240512081428 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create Cart table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $table = $schema->createTable('cart');
+        $table->addColumn('id', Types::STRING)->setLength(36)->setNotnull(true);
+        $table->setPrimaryKey(['id']);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $schema->dropTable('cart');
+    }
+}
