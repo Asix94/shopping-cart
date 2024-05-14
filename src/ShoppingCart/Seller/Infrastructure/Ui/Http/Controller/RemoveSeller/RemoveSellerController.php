8c539c0274305553e9cf868a0ca8c7bd250f7d80
Carlos Ramos <carlosrt1994@gmail.com>
2024-05-14 19:50:09 +0200

    feat: validate params when remove seller

--- src/ShoppingCart/Seller/Infrastructure/Ui/Http/Controller/RemoveSeller/RemoveSellerController.php
+++ src/ShoppingCart/Seller/Infrastructure/Ui/Http/Controller/RemoveSeller/RemoveSellerController.php
@@ -4,6 +4,7 @@ namespace App\ShoppingCart\Seller\Infrastructure\Ui\Http\Controller\RemoveSeller
 
 use App\ShoppingCart\Seller\Application\SellerEliminator;
 use App\ShoppingCart\Seller\Domain\Exceptions\FailedRemoveSellerException;
+use App\ShoppingCart\Shared\Domain\ValidateRequest;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -14,23 +15,16 @@ final class RemoveSellerController
     public function __invoke(Request $request): JsonResponse
     {
         try {
-            $id = $request->get('id');
-            $this->validateParam($id);
-            $sellerId = RemoveSellerRequest::sellerRequest($id);
-
+            ValidateRequest::validate($request, ['id']);
+            $sellerId = RemoveSellerRequest::sellerRequest(
+                $request->get('id')
+            );
             $this->eliminator->__invoke($sellerId);
             return new JsonResponse('Seller is remove successfully', 201);
         } catch (FailedRemoveSellerException $e) {
             return new JsonResponse($e->getMessage(), 500);
         } catch (\Exception $e) {
             return new JsonResponse($e->getMessage(), 404);
-        }
-    }
-
-    private function validateParam(string $nameParam): void
-    {
-        if (!$nameParam) {
-            throw new \Exception('Parameter ' . $nameParam . ' is required.');
         }
     }
 }
